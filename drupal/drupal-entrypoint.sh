3dcd6cc56367caceb11903353084a2c071403ad2
Sam Lachance <slachance@gmail.com>
2017-03-27 23:36:26 -0400

    added drupal 7.31 build files

---
+++ drupal/drupal-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+# wait for mysql to be available
+wait-for-it.sh $DRUPAL_DB_HOST:$DRUPAL_DB_PORT -t 45
+
+# preload the wordpress database
+mysql -h $DRUPAL_DB_HOST -u $DRUPAL_DB_USER -p$DRUPAL_DB_PASS $DRUPAL_DB_NAME < /usr/local/lib/penkit-drupal.sql
+
+exec php-entrypoint.sh
