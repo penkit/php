25b34743dcc68ecb5b4b6c0a40220fe370915f74
Sam Lachance <slachance@gmail.com>
2017-03-27 23:37:48 -0400

    typo in comment

--- drupal/drupal-entrypoint.sh
+++ drupal/drupal-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # wait for mysql to be available
 wait-for-it.sh $DRUPAL_DB_HOST:$DRUPAL_DB_PORT -t 45
 
-# preload the wordpress database
+# preload the drupal database
 mysql -h $DRUPAL_DB_HOST -u $DRUPAL_DB_USER -p$DRUPAL_DB_PASS $DRUPAL_DB_NAME < /usr/local/lib/penkit-drupal.sql
 
 exec php-entrypoint.sh
